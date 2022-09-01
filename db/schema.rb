9e00e9302b0ab782659878ff0f8c1e99485949d9
HSMathebula <56921581+HSMathebula@users.noreply.github.com>
2022-09-01 14:40:59 +0200

    Invocke relationship between users and comments

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_01_121246) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_01_123940) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,6 +18,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_01_121246) do
     t.text "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "likes", force: :cascade do |t|
@@ -33,8 +35,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_01_121246) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
-    t.bigint "author_id"
-    t.index ["author_id"], name: "index_posts_on_author_id"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
@@ -47,5 +47,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_01_121246) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "comments", "users"
   add_foreign_key "posts", "users"
 end
