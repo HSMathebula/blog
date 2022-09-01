dcd797098657722aeb7b52c44719afd24dd8bbed
HSMathebula <56921581+HSMathebula@users.noreply.github.com>
2022-09-01 14:54:54 +0200

    Invocke relationship between posts and likes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_01_124748) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_01_125206) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -28,6 +28,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_01_124748) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
+    t.bigint "post_id", null: false
+    t.index ["post_id"], name: "index_likes_on_post_id"
     t.index ["user_id"], name: "index_likes_on_user_id"
   end
 
@@ -53,6 +55,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_01_124748) do
 
   add_foreign_key "comments", "posts"
   add_foreign_key "comments", "users"
+  add_foreign_key "likes", "posts"
   add_foreign_key "likes", "users"
   add_foreign_key "posts", "users"
 end
