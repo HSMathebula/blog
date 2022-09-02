13ee00ef8c0f782152b86c32f1e842ed25bf139d
HSMathebula <56921581+HSMathebula@users.noreply.github.com>
2022-09-02 09:12:33 +0200

    Add test for posts_counter = 0 spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  subject { User.new(name: 'Huseyin', photo: 'google.com', bio: 'I was born in 2055', posts_counter: '5') }
+  subject { User.new(name: 'Happen', photo: 'google.com', bio: 'I was born in 1900', posts_counter: '5') }
   before { subject.save }
-  
+
   it 'name should be present' do
     subject.name = nil
     expect(subject).to_not be_valid
@@ -12,5 +12,10 @@ RSpec.describe User, type: :model do
   it 'posts counter should be greater than 0' do
     subject.posts_counter = -5
     expect(subject).to_not be_valid
+  end
+
+  it 'posts counter should be equal to 0' do
+    subject.posts_counter = 0
+    expect(subject).to be_valid
   end
 end
