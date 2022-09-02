20a47a3da4982b6a752c6d1bee4d8c893bb2e278
HSMathebula <56921581+HSMathebula@users.noreply.github.com>
2022-09-02 09:10:50 +0200

    Add test for posts_counter numericality spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,8 +3,14 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   subject { User.new(name: 'Huseyin', photo: 'google.com', bio: 'I was born in 2055', posts_counter: '5') }
   before { subject.save }
+  
   it 'name should be present' do
     subject.name = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'posts counter should be greater than 0' do
+    subject.posts_counter = -5
     expect(subject).to_not be_valid
   end
 end
