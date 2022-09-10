9eee437a841823245200e53433e41f867b82ef1c
HSMathebula <56921581+HSMathebula@users.noreply.github.com>
2022-09-10 13:56:49 +0200

    Fix linter errors

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,7 +1,7 @@
 class LikesController < ApplicationController
   def create
     @like = Like.new(post_id: params[:post_id], author_id: current_user.id)
-    
+
     if @like.save
       @like.update_likes_counter
       flash[:success] = 'Post created successfully.'
