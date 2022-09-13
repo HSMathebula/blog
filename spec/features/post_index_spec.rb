7d383fc4a0595c4c8d5411c3d5c94e51251f0bf5
HSMathebula <56921581+HSMathebula@users.noreply.github.com>
2022-09-13 22:37:39 +0200

    Change post index spec code and add test for user index spec

--- spec/features/post_index_spec.rb
+++ spec/features/post_index_spec.rb
@@ -1,25 +1,38 @@
 require 'rails_helper'
-
-RSpec.describe 'Users', type: :feature do
-  describe 'user#index' do
+describe 'Posts-index', type: :feature do
+  describe 'users/posts/index' do
     before(:each) do
-      User.create! name: 'Happen', photo: 'https://unsplash.com/photos/l-T-LpQnNRg', bio: 'developper', posts_counter: 0
-      visit root_path
-      @user = User.first!
-    end
+      @user_one = User.create! name: 'Tom', photo: 'https://unsplash.com/photos/l-T-LpQnNRg', bio: 'designer', posts_counter: 0
+      @user_two = User.create! name: 'Happen', photo: 'https://unsplash.com/photos/l-T-LpQnNRg', bio: 'developper', posts_counter: 0
 
-    scenario 'I can see the username of all other users.' do
-      expect(page).to have_content('Happen')
+      @post1 = @user_one.posts.create!(title: 'first post', text: 'description', comments_counter: 0, likes_counter: 0)
+      @post2 = @user_one.posts.create!(title: 'second post', text: 'description', comments_counter: 0, likes_counter: 0)
+      @comment_one = @post1.comments.create!(text: 'comment one', author_id: @user_one.id)
+      @comment_two = @post1.comments.create!(text: 'comment two', author_id: @user_two.id)
+
+      visit(user_posts_path(@user_one.id))
     end
-    scenario 'I can see the profile picture for each user.' do
-      expect(page).to have_selector('.user_picture')
+    scenario 'I can see the users profile picture.' do
+      expect(page).to have_selector('span')
     end
-    scenario 'I can see the number of posts each user has written.' do
-      expect(page).to have_content('Number of Posts: 0')
+    scenario "I can see the user's username" do
+      expect(page).to have_content('Tom')
     end
-    scenario "When I click on a user, I am redirected to that user's show page" do
-      visit(user_path(@user.id))
-      expect(page).to have_content('Bio')
+    scenario "I can see some of the post's body." do
+      expect(page).to have_content('description')
+    end
+    scenario 'I can see the first comments on a post.' do
+      expect(page).to have_content('comment one')
+    end
+    scenario 'I can see how many comments a post has.' do
+      expect(page).to have_content('Comments: 2')
+    end
+    scenario 'I can see how many likes a post has.' do
+      expect(page).to have_content('Likes: 0')
+    end
+    it "When I click on a post, it redirects me to that post's show page." do
+      visit(user_post_path(@user_one.id, @post1.id))
+      expect(page).to have_current_path user_post_path(@user_one.id, @post1.id)
     end
   end
 end
